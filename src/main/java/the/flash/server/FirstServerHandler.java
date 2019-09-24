d394c7de989a0696cc0ec6793c075c73ca9218cd
Mr.Zheng <volatile@126.com>
2019-09-24 16:35:06 +0800

    格式化代码

--- src/main/java/the/flash/server/FirstServerHandler.java
+++ src/main/java/the/flash/server/FirstServerHandler.java
@@ -3,7 +3,6 @@ package the.flash.server;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-
 import java.nio.charset.Charset;
 import java.util.Date;
 
@@ -14,25 +13,25 @@ import java.util.Date;
  */
 public class FirstServerHandler extends ChannelInboundHandlerAdapter {
 
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        ByteBuf byteBuf = (ByteBuf) msg;
+  @Override
+  public void channelRead(ChannelHandlerContext ctx, Object msg) {
+    ByteBuf byteBuf = (ByteBuf) msg;
 
-        System.out.println(new Date() + ": 服务端读到数据 -> " + byteBuf.toString(Charset.forName("utf-8")));
+    System.out.println(new Date() + ": 服务端读到数据 -> " + byteBuf.toString(Charset.forName("utf-8")));
 
-        // 回复数据到客户端
-        System.out.println(new Date() + ": 服务端写出数据");
-        ByteBuf out = getByteBuf(ctx);
-        ctx.channel().writeAndFlush(out);
-    }
+    // 回复数据到客户端
+    System.out.println(new Date() + ": 服务端写出数据");
+    ByteBuf out = getByteBuf(ctx);
+    ctx.channel().writeAndFlush(out);
+  }
 
-    private ByteBuf getByteBuf(ChannelHandlerContext ctx) {
-        byte[] bytes = "你好，欢迎关注我的微信公众号，《闪电侠的博客》!".getBytes(Charset.forName("utf-8"));
+  private ByteBuf getByteBuf(ChannelHandlerContext ctx) {
+    byte[] bytes = "hello,你好".getBytes(Charset.forName("utf-8"));
 
-        ByteBuf buffer = ctx.alloc().buffer();
+    ByteBuf buffer = ctx.alloc().buffer();
 
-        buffer.writeBytes(bytes);
+    buffer.writeBytes(bytes);
 
-        return buffer;
-    }
+    return buffer;
+  }
 }
