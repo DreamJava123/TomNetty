6295ff7767d96e720eb7370290e62e229de7f206
Mr.Zheng <volatile@126.com>
2019-09-26 01:25:37 +0800

    登录失败增加响应

--- src/main/java/the/flash/server/handler/inbound/LoginCheck.java
+++ src/main/java/the/flash/server/handler/inbound/LoginCheck.java
@@ -17,17 +17,18 @@ public class LoginCheck extends ChannelInboundHandlerAdapter {
       //交给别的线程去做 不阻塞线程?? 还是当前handle去做? 还是缓存校验? 还是通过channel去做校验
       LoginRequestPacket loginRequestPacket = (LoginRequestPacket) msg;
       if (!loginRequestPacket.getPassword().equalsIgnoreCase("dsada")) {
+        LoginResponsePacket loginResponsePacket = new LoginResponsePacket();
+        loginResponsePacket.setSuccess(false);
+        loginResponsePacket.setReason("密码不对 请重新输入!!!");
+        //妈的 一定要在channel关闭前 将最后的信息发送出去 要不然链接就关闭了
+        ByteBuf encode = PacketCodeC.INSTANCE.encode(ctx.channel().alloc(), loginResponsePacket);
+        ctx.channel().writeAndFlush(encode);
         ctx.channel().close().addListener(future -> {
           //监听回调方法
           if (future.isSuccess()) {
             System.out.println("close success!!!!");
           }
         });
-        LoginResponsePacket loginResponsePacket = new LoginResponsePacket();
-        loginResponsePacket.setSuccess(false);
-        loginResponsePacket.setReason("密码不对 请重新输入!!!");
-        ByteBuf encode = PacketCodeC.INSTANCE.encode(ctx.channel().alloc(), loginResponsePacket);
-        ctx.channel().writeAndFlush(encode);
       } else {
         ctx.fireChannelRead(msg);
         System.out.println(loginRequestPacket.toString());
