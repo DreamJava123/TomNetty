704dfb021c1b3037e74c19c98e5c9d6fc895e37f
Mr.Zheng <volatile@126.com>
2019-09-26 00:32:42 +0800

    入参魔数校验

--- src/main/java/the/flash/server/handler/TomCodec.java
+++ src/main/java/the/flash/server/handler/TomCodec.java
@@ -2,25 +2,31 @@ package the.flash.server.handler;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageDecoder;
-import java.util.List;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 
 /**
  * Created by TOM
  * On 2019/9/24 16:31
  */
-public class TomCodec extends ByteToMessageDecoder {
+public class TomCodec extends LengthFieldBasedFrameDecoder {
 
   private static final int MAGIC_NUMBER = 0x12345678;
+  //数据包协议偏移量
+  private static final int lengthFieldOffset = 7;
+  //协议请求data 字节长度 我这边定义的还是一个int值长度
+  private static final int lengthFieldLength = 4;
+
+  public TomCodec() {
+    super(Integer.MAX_VALUE, lengthFieldOffset, lengthFieldLength);
+  }
 
   @Override
-  protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
-    System.out.println(MAGIC_NUMBER);
-    System.out.println(in.getInt(0));
-    if (in.getInt(0) != MAGIC_NUMBER) {
+  protected Object decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
+    if (in.getInt(in.readerIndex()) != MAGIC_NUMBER) {
+      //校验请求头魔数 不是此协议 关闭连接
       ctx.channel().close();
-      return;
+      return null;
     }
-    ctx.fireChannelRead(in);
+    return super.decode(ctx, in);
   }
 }
